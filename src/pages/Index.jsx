07f51c854e423d636024e1820309d0d3aac03acc
Marcos <marsanmar6291@gmail.com>
2023-02-02 16:36:08 -0600

    first commit

---
+++ src/pages/Index.jsx
@@ -0,0 +1,46 @@
+import { useLoaderData } from "react-router-dom";
+import Cliente from "../components/Cliente";
+import { obtenerClientes } from "../data/Clientes";
+
+export function loader(){
+    const clientes = obtenerClientes()
+
+    return clientes
+}
+
+function Index() {
+  
+  const clientes = useLoaderData();
+
+  return (
+    <>
+      <h1 className="text-4xl text-blue-900 font-black">Clientes</h1>
+      <p className="mt-3">Administra tus clientes</p>
+
+      {clientes.length > 0 ? (
+        <table className="w-full bg-white shadow mt-5 table-auto">
+          <thead className="bg-blue-800 text-white">
+            <tr>
+              <th className="p-2">Cliente</th>
+              <th className="p-2">Contacto</th>
+              <th className="p-2">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {clientes.map( cliente => (
+             <Cliente 
+                cliente={cliente}
+                key={cliente.id}
+              />
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>NO hay clientes aun</p>
+      )}
+    </>
+
+  )
+}
+
+export default Index
